38995c8d577b072e980ff1d838a04e16d7994dd0
Gautier MICHELIN <gm@ideesculture.com>
2023-01-10 10:49:55 +0100

    adding a test view for wavesurfer

--- src/views/MainRecordView.vue
+++ src/views/MainRecordView.vue
@@ -1,9 +1,7 @@
-<script setup>
-
-</script>
-
 <template>
   <div id="top"></div>
+  <div ref="wave2" id="wave2"></div>
+  <div ref="wave3" id="wave3" @click="test"></div>	
   <div id="bottom">
     <div id="topbottomright">
       Source d'entrée : unprocessed<br />
@@ -12,16 +10,158 @@
     <div id="topbottomleft">Gain en amplitude : 0.0dB</div>
 
     <div style="text-align: center; margin-top: 20px">
-      <button id="microphone">
+      <button id="microphone" v-if="micoff" v-on:click="openmic">
         <font-awesome-icon icon="microphone" />
+      </button>
+      <button id="stopmicrophone" v-if="micon" @click="stopmic">
+        <font-awesome-icon icon="stop" />
       </button>
     </div>
 
-    <div id="bottomproperties">2022-10-03 14:50:20</div>
+    <div id="bottomproperties" v-html="bottomproperties"></div>
   </div>
 </template>
+<script>
+import WaveSurfer from "wavesurfer.js";
+import Microphone from "wavesurfer.js/dist/plugin/wavesurfer.microphone";
+
+export default {
+  components: {},
+  data() {
+    return {
+      count: 0,
+      wavesurfer: null,
+			wavesurfer2: null,
+      wavesurferCurrent: null,
+      spectrogram: null,
+      microphone: {},
+      context: {},
+      processor: {},
+      micon: false,
+      micoff: true,
+      recording: false,
+      mediaRecorder: null,
+      audioChunks: [],
+      PtDialogIsOpen: true,
+      bottomproperties: "-",
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    openmic() {
+      this.micon = true;
+      this.micoff = false;
+      this.recording = true;
+      var AudioContext = window.AudioContext || window.webkitAudioContext;
+      this.context = new AudioContext();
+      this.processor = this.context.createScriptProcessor(1024, 1, 1);
+      if (this.wavesurfer) {
+        this.wavesurfer.destroy();
+      }
+      this.$nextTick(() => {
+        this.wavesurfer = WaveSurfer.create({
+          container: this.$refs.wave2,
+          waveColor: "#31A9F9",
+          progressColor: "#848383",
+          interact: true,
+          audioContext: this.context || null,
+          audioScriptProcessor: this.processor || null,
+          plugins: [Microphone.create()],
+        });
+        this.wavesurfer.microphone.on("deviceReady", function (stream) {
+          console.info("Device ready!");
+          console.info(stream);
+          let mediaRecorder = new MediaRecorder(stream);
+          let audioChunks = [];
+          //console.log(mediaRecorder);
+          mediaRecorder.start();
+          mediaRecorder.addEventListener("dataavailable", (event) => {
+            audioChunks.push(event.data);
+          });
+          mediaRecorder.addEventListener("stop", () => {
+            console.log("stop");
+            const audioBlob = new Blob(audioChunks);
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+          });
+        });
+        this.wavesurfer.microphone.on("deviceError", function (code) {
+          console.warn("Device error: " + code);
+        });
+        this.wavesurfer.microphone.start();
+      });
+    },
+    select() {},
+    startmic() {
+      this.wavesurfer.microphone.start();
+      this.recording = true;
+    },
+    stopdevicemic() {
+      this.wavesurfer.microphone.stopDevice();
+      this.recording = false;
+    },
+    playmic() {
+      this.wavesurfer.microphone.play();
+    },
+    pausemic() {
+      this.wavesurfer.microphone.pause();
+    },
+    stopmic() {
+      this.wavesurfer.microphone.stop();
+      this.micon = false;
+      this.micoff = true;
+      this.bottomproperties = new Date().toLocaleString();
+    },
+    toggleplaymic() {
+      this.wavesurfer.microphone.togglePlay();
+    },
+		test() {
+			this.wavesurfer2 = WaveSurfer.create({
+				container: this.$refs.wave3,
+				barWidth: 3
+			});
+      this.wavesurfer2.load(
+        "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+      );
+ 
+		}
+  },
+  mounted() {
+    this.micon = false;
+    this.micoff = true;
+ },
+};
+</script>
 
 <style scoped>
+.audioRecordDiv {
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  margin-bottom: 8px;
+  padding: 12px;
+}
+.audioRecordLi {
+  list-style-type: none;
+  background-color: #d8d8d8;
+  display: block;
+  padding: 12px;
+}
+.audioRecordLi .title {
+  font-weight: bold;
+  color: green;
+  font-size: 20px;
+}
+.audioRecordLi .date {
+  display: inline-block;
+  float: right;
+}
+.audioRecordLi audio {
+  width: 100%;
+}
+
 #top {
   height: 40vh;
   background-color: #009b60;
@@ -36,16 +176,25 @@
 }
 #topbottomright,
 #topbottomleft {
-	font-size:12px;
+  font-size: 12px;
 }
 #microphone {
   height: 120px;
   width: 120px;
   border-radius: 120px;
   font-size: 40px;
-	background-color:red;
-	color:white;
-	border:1px solid darkred;
+  background-color: red;
+  color: white;
+  border: 1px solid darkred;
+}
+#stopmicrophone {
+  height: 120px;
+  width: 120px;
+  border-radius: 120px;
+  font-size: 40px;
+  background-color: #222;
+  color: white;
+  border: 1px solid #111;
 }
 #bottomproperties {
   position: absolute;
@@ -53,5 +202,15 @@
   left: 20px;
   right: 20px;
   text-align: center;
+}
+#wave2 {
+  height: 180px;
+  background-color: lightgray;
+  width: 100%;
+}
+#wave3 {
+  height: 180px;
+  background-color: lightcoral;
+  width: 100%;
 }
 </style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>Ici</div>
+    <div id="waveform" ref="waveform"></div>
+  </div>
+</template>
+
+<script>
+import WaveSurfer from "wavesurfer.js";
+
+export default {
+  name: "TestView",
+  data: () => ({
+    wavesurfer: null,
+  }),
+  async mounted() {
+    if (!this.wavesurfer) this.createWaveSurfer();
+  },
+  methods: {
+    createWaveSurfer() {
+      this.wavesurfer = WaveSurfer.create({
+        container: "#waveform",
+        barWidth: 3,
+      });
+      this.wavesurfer.load(
+        "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+      );
+    },
+  },
+};
+</script>
