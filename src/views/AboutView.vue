6aa8cbe91a202c7bf2de77db35357bbce71f1086
AndreasAutin <autinandreas@outlook.com>
2023-02-06 17:47:48 +0100

    ajout texte a propos un

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,55 @@
   <div class="about">
 	<div>
 		<h1>A propos de LutherieTools</h1>
-		<p>Ceci est le contenu de ma page A propos.</p>
+		<p>
+      <br>
+      <br>
+      Nous sommes une équipe d’étudiant ingénieur et enseignant chercheur qui ont travaillés ensemble sur un projet de dernière année d’école d’ingénieur à L’ENSIM
+       (Le Mans 72000, France). 
+      Le Projet LutherieTools à pour but d’aider et d’ameliorer le travail des luthiers lors de leurs conceptions de leurs intruments. 
+      Grâce aux analyses des sons de leurs instruments les luthiers peuvent precevoir leurs <strong> modes de corps et de cordes</strong>. 
+      Vous avez une question sur l’applicaton ? N'hésitez pas à nous contacter!
+      <br>
+      <br>
+
+      <U> Les étudiants :  </U>
+
+      <ul>
+      <li><strong>Benjamin Quiedeville</strong>
+      <br>
+      <U>Contact :</U> email, linkedin, GitHub. . .</li>
+      <li> <strong>Clément Larcade</strong>
+      <br>
+      <U>Contact :</U> email, linkedin, GitHub. . .</li>
+      <li> <strong> Andréas Autin </strong>
+      <br>
+      <U>Contact :</U> email, linkedin, GitHub. . .
+      </li>
+      </ul>
+
+      <br>
+      <br>
+
+      <U> Les enseignants :  </U>
+
+      <ul>
+      <li><strong>François Gautier </strong>
+      <br>
+      <U>Contact :</U> email, linkedin, GitHub. . .</li>
+      <li> <strong>Frédéric Ablitzer</strong>
+      <br>
+      <U>Contact :</U> email, linkedin, GitHub. . .</li>
+      <li> <strong> Gautier Michelin </strong>
+      <br>
+      <U>Contact :</U> email, linkedin, GitHub. . .
+      </li>
+      </ul>
+
+      <br>
+      <br>
+
+
+    </p>
 	</div>
   </div>
 </template>
