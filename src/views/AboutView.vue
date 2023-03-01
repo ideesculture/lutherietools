f47ee7636a249107ab9b4a591d1b4081bd18901d
Benjamin Quiedeville <quiedevilleb@gmail.com>
2023-03-01 13:50:56 +0100

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -59,45 +59,48 @@
       </table>
 
       <U> Les enseignants :  </U>
-
-      <br>
-        <img src="img/FGautier.jpg"
+      <table>
+  
+      <tr> 
+        <th>
+          <img src="img/FGautier.jpg"
           alt="Photo de M. Gautier "
           width="200"
           height="200">
-      <br>
-
-      
-      <strong>François Gautier </strong>
-      <br>
-      <U>Contact :</U> email, linkedin, GitHub. . .
-
-      <br>
-        <img src="img/FAblitzer.jpg"
+        </th> 
+        <th>
+          <img src="img/FAblitzer.jpg"
           alt="Photo de M. Ablitzer "
           width="200"
           height="200">
-      <br>
-
-      <strong>Frédéric Ablitzer</strong>
-      <br>
-      <U>Contact :</U> email, linkedin, GitHub. . .
-
-      <br>
-        <img src="img/GMichelin.jpg"
+          </th> 
+          <th>
+            <img src="img/GMichelin.jpg"
           alt="Photo de M. Michelin "
           width="200"
           height="200">
-      <br>
+        </th> 
+      </tr>
 
-      <strong> Gautier Michelin </strong>
-      <br>
-      <U>Contact :</U> email, linkedin, GitHub. . .
-      
-      
+      <tr> 
+        <td>
+          <strong>François Gautier </strong>
+          <br>
+          <U>Contact :</U> email, linkedin, GitHub. . ..
+        </td>
+        <td>
+          <strong>Frédéric Ablitzer</strong>
+          <br>
+          <U>Contact :</U> email, linkedin, GitHub. . .
+        </td> 
+        <td>
+          <strong> Gautier Michelin </strong>
+          <br>
+          <U>Contact :</U> email, linkedin, GitHub. . .
+        </td> 
+      </tr>
 
-      <br>
-      <br>
+      </table>
 
 
     </p>
